0536de4386a716f3ab6fb1e56801dd62f3e94ac5
Brock <brockboe@gmail.com>
2019-12-13 17:49:48 -0600

    Added comments to python notebooks

--- linear_layer.runs/impl_1/rundef.js
+++ linear_layer.runs/impl_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "init_design", "begin" );
+ISETouchFile( "write_bitstream", "begin" );
 ISEStep( "vivado",
          "-log system_control_wrapper.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source system_control_wrapper.tcl -notrace" );
 
